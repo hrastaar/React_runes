2b69350efbcc6967fa3d259436055560e43fdf22
Auraofdivinity <peirisasel373@gmail.com>
2019-09-19 11:07:36 +0530

    Updates

---
+++ precache-manifest.ee5759142dadd028d3d5a026ce99e8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45c90664fb89fcc85838542f2991cf0",
+    "url": "/React-runes/index.html"
+  },
+  {
+    "revision": "9275219b658b4d1076bd",
+    "url": "/React-runes/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9178bbc6bc22658e88d3",
+    "url": "/React-runes/static/js/2.d6c30f2a.chunk.js"
+  },
+  {
+    "revision": "9275219b658b4d1076bd",
+    "url": "/React-runes/static/js/main.dab29728.chunk.js"
+  },
+  {
+    "revision": "8dc34147513b90d8fd2f",
+    "url": "/React-runes/static/js/runtime~main.7e06ca4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-runes/static/media/logo.5d5d9eef.svg"
+  }
+]);
