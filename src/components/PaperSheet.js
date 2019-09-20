5af5621ba7bdda7ed5e0a1fe6c29b37582eb8bf6
Auraofdivinity <peirisasel373@gmail.com>
2019-09-20 07:40:51 +0530

    moving loadData function into Paper component

--- src/components/PaperSheet.js
+++ src/components/PaperSheet.js
@@ -6,8 +6,42 @@ import poppins from 'typeface-poppins'
 
 
 class PaperSheet extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  
 render(){
-  console.log(this.props.summoner)
+  const searchBtn = document.getElementById('search-btn')
+  searchBtn.onclick  = function(){
+    loadData().then(res=> {
+      this.setStae = {
+        summonerObject: res
+      }
+    })
+  }
+
+  function loadData() {
+    const url =
+      'https://corsproxy2213.herokuapp.com/?q=https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-77f1c292-674d-4fdd-8ecc-a495a3bdbf01';
+    //implement fetch here
+    return new Promise(function (resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url)
+
+      req.onload = function () {
+        if (req.status == 200) {
+          console.log(req.response)
+          document.getElementById('root').style.display = 'block'
+          resolve(req.response)
+        } else {
+
+          reject(alert("The summoner you serached for doesn't appear to be in a game"))
+        }
+      };
+      req.send();
+    });
+  }
+  
   return (
     <div>
       <Paper>
